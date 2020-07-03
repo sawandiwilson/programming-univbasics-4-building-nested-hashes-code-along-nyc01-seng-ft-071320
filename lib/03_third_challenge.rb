759db0c4edf948d428d5f748045835610db4572a
Sawandi Wilson <sawandiwilson@gmail.com>
2020-07-03 16:15:41 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -32,8 +32,9 @@ def third_challenge
       :age => "50"
     },
       :matriarch => {
-        #your key/value pairs here
-        },
+        name => "Lady Capulet",
+      :age => "51"
+    },
       :heroine => {
         #your key/value pairs here
         },
